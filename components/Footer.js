df837a2b377de2f57543a56c9222f8b9681691c1
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-04 22:18:32 +0100

    Layout: Header & Footer components

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>Copyright &copy; BreathTaking Events {new Date().getFullYear()}</p>
+            <p>
+                <Link href="/about">About This Project</Link>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
