547a7fbdd27cc3f8a9e474553ba319b38c71e77c
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-05 14:56:33 +0100

    getServerSideProps & simple next api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout';
+import {API_URL} from '@/config/index.js';
 
-export default function HomePage() {
+export default function HomePage({events}) {
+    console.log(events);
     return (
         <Layout>
-            <h1>Home</h1>
+            <h1>Upcoming Events</h1>
         </Layout>
     )
+};
+
+export async function getServerSideProps () {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: {events}
+    }
 }
