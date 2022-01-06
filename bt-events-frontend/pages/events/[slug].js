a65163b3546506b006c34f80cd7b4559225d7547
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-06 21:27:09 +0100

    delete event function

--- bt-events-frontend/pages/events/[slug].js
+++ bt-events-frontend/pages/events/[slug].js
@@ -1,13 +1,31 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import {ToastContainer, toast} from 'react-toastify';
+import {useRouter} from 'next/router';
+import 'react-toastify/dist/ReactToastify.css';
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config/index";
 import styles from '@/styles/Event.module.css';
+import React from "react";
 
 const EventPage = ({evt}) => {
-    const deleteEvent = (e) => {
-        console.log('delete');
+    const router = useRouter();
+
+    const deleteEvent = async (e) => {
+        if (confirm('Are you sure?')) {
+            const res = await fetch(`${API_URL}/api/events/${evt.id}`, {
+                method: 'DELETE'
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                toast.error(data.message)
+            } else {
+                router.push('/events');
+            }
+        }
     };
 
     return (
@@ -52,6 +70,18 @@ const EventPage = ({evt}) => {
                     </a>
                 </Link>
             </div>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </Layout>
     );
 };
