fae5882d858d2fff3c17245ff2c55513fa776fd2
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-07 17:09:53 +0100

    store JWT in Server HttpOnly Cookie

--- bt-events-frontend/context/AuthContext.js
+++ bt-events-frontend/context/AuthContext.js
@@ -23,8 +23,6 @@ export const AuthProvider = ({children}) => {
 
         const data = await res.json();
 
-        console.log(data);
-
         if (res.ok) {
             setUser(data.user);
         } else {
--- bt-events-frontend/pages/events/edit/[id].js
+++ bt-events-frontend/pages/events/edit/[id].js
@@ -229,9 +229,11 @@ const EditEventPage = ({evt}) => {
     );
 };
 
-export async function getServerSideProps({params: {id}}) {
+export async function getServerSideProps({params: {id}, req}) {
     const res = await fetch(`${API_URL}/api/events/${id}?populate=image`);
     const evt = await res.json();
+
+    console.log(req.headers.cookie);
 
     return {
         props: {
