06a9fd3dfbf43f065e7a86487bd46c1ec9400105
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-07 14:42:48 +0100

    Pagination component

--- bt-events-frontend/pages/events/index.js
+++ bt-events-frontend/pages/events/index.js
@@ -1,9 +1,9 @@
 import Layout from '@/components/Layout';
 import EventItem from "@/components/EventItem";
-import {API_URL} from '@/config/index.js';
+import {API_URL, PER_PAGE} from '@/config/index.js';
+import Pagination from "@/components/Pagination";
 
-export default function EventsPage({events}) {
-
+export default function EventsPage({events, page, total}) {
     return (
         <Layout>
             <h1>Events</h1>
@@ -11,15 +11,22 @@ export default function EventsPage({events}) {
             {events.map(evt => (
                 <EventItem key={evt.id} evt={evt} />
             ))}
+
+            <Pagination page={page} total={total}/>
         </Layout>
     )
 };
 
-export async function getServerSideProps () {
-    const res = await fetch(`${API_URL}/api/events?populate=image&sort=date:ASC`);
+export async function getServerSideProps ({query: {page = 1}}) {
+
+    const res = await fetch(`${API_URL}/api/events?populate=image&pagination[page]=${page}&pagination[pageSize]=${PER_PAGE}`);
     const events = await res.json();
 
     return {
-        props: {events: events.data}
+        props: {
+            events: events.data,
+            page: +page,
+            total: events.meta.pagination.total
+        }
     }
 }
